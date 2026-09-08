584bb94f3afb2806e0cdc0328ec7743ddbc80de5
TRL <138390412+TRLRD@users.noreply.github.com>
2026-09-08 14:36:52 +0300

    fix: repair launcher syntax error

--- launcher.py
+++ launcher.py
@@ -43,7 +43,7 @@ def _replace_closure_cell(function, predicate, replacement):
         return function
 
     cells = list(closure)
-    for index, cell in enumerate(cells):
+    for index, cell in enumerate(closure):
         try:
             value = cell.cell_contents
         except ValueError:
